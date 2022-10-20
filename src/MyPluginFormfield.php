6417d0ee2e233549e4418ff2e95444ffd600b439
Christoph Schweppe <info@cschweppe.de>
2022-10-20 15:39:26 +0200

    Update MyPluginFormfield.php

--- src/MyPluginFormfield.php
+++ src/MyPluginFormfield.php
@@ -6,27 +6,42 @@ use Voyager\Admin\Classes\Formfield;
 
 class MyPluginFormfield extends Formfield
 {
-    public function type(): string
-    {
-        return 'my-plugin';
+    // Remove any of the following properties when necessary.
+    public $notTranslatable;        // Formfield can not be translated
+    public $notAsSetting;           // Formfield can not be used as a setting
+    public $notInLists;             // Can not be used in Lists
+    public $notInViews;             // Can not be used in Views
+    public $browseArray;            // Get the data as an array when browsing
+    public $noColumns;              // Don't allow normal columns as the field
+    public $noComputedProps;        // Don't allow accessors as the field
+    public $noRelationships;        // Don't allow relationship objects as the field
+    public $noRelationshipProps;    // Don't allow relationship columns as the field
+    public $noRelationshipPivots;   // Don't allow relationship pivot columns as the field
+
+    // The name of your formfield in a readable form. Can be a translation object
+    public function name(): string {
+        return 'My Formfield';
+        // Or
+        return __('mynamespace::name');
     }
 
-    public function name(): string
-    {
-        return 'MyPlugin';
+    // The name in a slugged form, used for identification. Can not be translated!
+    public function type(): string {
+        return 'my-formfield';
     }
 
-    public function browse(mixed $input): mixed
-    {
+    // The name of the component
+    public function getComponentName(): string {
+        return 'my-formfield';
+    }
+
+    // The name of the builder component
+    public function getBuilderComponentName(): string {
+        return 'my-formfield-builder';
+    }
+
+    public function browse(mixed $input): mixed {
         // Manipulate incoming browse data
         return $input;
-    }
-
-    public function getComponentName(): string {
-        return 'my-plugin-formfield';
-    }
-
-    public function getBuilderComponentName(): string {
-        return 'my-plugin-builder';
     }
 }
